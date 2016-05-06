756fc77d1ebe5bccea73fd2ae4b9b54fb30a06c2
asullom <asullom@gmail.com>
2016-05-06 12:01:58 -0500

    initial

---
+++ demo5_project/authx/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+
+    nombre = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
